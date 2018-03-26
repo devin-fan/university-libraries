d184c7857272b01b85f75c5f0f27b83e979d8391
thepavangollapalli1 <pavankgollapalli@gmail.com>
2018-03-25 22:01:58 -0400

    Added download and params methods

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -1,5 +1,5 @@
 class FilmController < ApplicationController
-    before_action :set_film, only: [:show]
+    before_action :set_film, only: [:show, :download]
      
     def index
         @base_films = Film.base_films.alphabetical
@@ -7,10 +7,24 @@ class FilmController < ApplicationController
       
     def show
     end
+
+    def download
+        send_file(@film.film_path, type: "application/mov")
+    end
+
+    def new
+    end
+
+    def create
+    end
     
     private 
     def set_film
         @film = Film.find_params[:id]
+    end
+
+    def film_params
+        params.require(:film).permit(:title, :tags, :essay_path, :type, :director, :film_path, :permission, :description)
     end
         
 end
