b5d33256a2d91db677189028eecc29174f47bdcf
thepavangollapalli1 <pavankgollapalli@gmail.com>
2018-04-01 23:25:13 -0400

    Fixed relationships so db:setup works now

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,9 +1,10 @@
 class Film < ActiveRecord::Base
+    has_many :film_tags
     has_many :tags, through: :film_tags
     mount_uploader :student_film, StudentFilmUploader
     mount_uploader :essay, EssayUploader
 
-    validates_presence_of :title, :tags, :essay_path, :type, :director, :film_path, :description
+    validates_presence_of :title, :tags, :essay_path, :film_type, :director, :film_path, :description
     validates_inclusion_of :permission, in: [0,1,2]
 
     scope :alphabetical,  -> { order(title: :asc) }
