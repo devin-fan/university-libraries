35450e393def797264df4fb2ddff372915c704a1
thepavangollapalli1 <pavankgollapalli@gmail.com>
2018-04-01 21:43:15 -0400

    Updated relationships for models and worked on create method in controller

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,5 +1,8 @@
 class Film < ActiveRecord::Base
+    has_many :tags, through: :film_tags
     mount_uploader :student_film, StudentFilmUploader
+
+    
 
     scope :alphabetical,  -> { order(title: :asc) }
     scope :base_films,    -> { where(film_type: 1) } # type 1 is base, type 2 is student
