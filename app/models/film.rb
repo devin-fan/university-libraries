c983c41927438e52deccb1f601e297d885cf9898
thepavangollapalli1 <pavankgollapalli@gmail.com>
2018-04-01 22:40:49 -0400

    Fixing controller and validations and added essay uploader

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,10 +1,12 @@
 class Film < ActiveRecord::Base
     has_many :tags, through: :film_tags
     mount_uploader :student_film, StudentFilmUploader
+    mount_uploader :essay, EssayUploader
 
-    
+    validates_presence_of :title, :tags, :essay_path, :type, :director, :film_path, :description
+    validates_inclusion_of :permission, in: [0,1,2]
 
     scope :alphabetical,  -> { order(title: :asc) }
-    scope :base_films,    -> { where(film_type: 1) } # type 1 is base, type 2 is student
-    scope :student_films, -> { where(film_type: 2) }
+    scope :base_films,    -> { where(film_type: 0) } # type 1 is base, type 2 is student
+    scope :student_films, -> { where(film_type: 1) }
 end
