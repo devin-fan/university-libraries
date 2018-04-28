17275a5b18b4ceff4d3f8262cec9103af844acda
Wolf <obedappiah-agyeman@Obeds-MBP.fios-router.home>
2018-04-27 20:42:41 -0400

    JS changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,18 +34,18 @@
 
 
 
-    $(window).bind("load", function () {
-	  var footer = $("#page-footer");
-	  var pos = footer.position();
-	  var height = $(window).height();
-	  height = height - pos.top;
-	  height = height - footer.height();
-	  if (height > 0) {
-	      footer.css({
-	          'margin-top': height + 'px'
-	      });
-	  }
-	});
+ //    $(window).bind("load", function () {
+	//   var footer = $("#page-footer");
+	//   var pos = footer.position();
+	//   var height = $(window).height();
+	//   height = height - pos.top;
+	//   height = height - footer.height();
+	//   if (height > 0) {
+	//       footer.css({
+	//           'margin-top': height + 'px'
+	//       });
+	//   }
+	// });
 
 
   });
